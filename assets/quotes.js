077528f4c84ac667498d79f88c0c45fa2f710845
VjRusmayana <RusmayanaHacked@gmail.com>
2020-05-08 00:41:08 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -2,7 +2,7 @@
 export default [
     { author: "Vj Rusmayana", quotes: "hey kamu, kapan nih ngabuburit bareng ❤" },
     { author: "Vj Rusmayana", quotes: "hey kamu, kapan nih meet ga kangen apa ?" },
-    { author: "Vj Rusmayana", quotes: "hey kamu, kapan nih Ibuku dan ibumu menjadi ibukita :*" },
+    { author: "Vj Rusmayana", quotes: "hey kamu, kapan nih Ibuku dan ibumu menjadi ibukita ❤" },
     { author: "Vj Rusmayana", quotes: "I love you." },
     { author: "Vj Rusmayana", quotes: "I may not with you everyday, but I love you everyday." },
     { author: "Vj Rusmayana", quotes: "I love you." },
